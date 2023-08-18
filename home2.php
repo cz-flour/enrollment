f0215a1f8fc87a7c944201d5f8a673b4623deadc
Czarina Aicah Lopez <128132912+cz-flour@users.noreply.github.com>
2023-08-18 20:49:38 +0800

    Add files via upload

---
+++ home2.php
@@ -0,0 +1,81 @@
+
+<!DOCTYPE html lang=en>
+<html>
+    
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="style.css"> 
+    <title>Our Lady of the Roses Montessori Learning Center</title>
+    <link rel="icon" href="logo.png">   
+    <!-- <link  type="text/css" href="./plugins/bootstrap.min.css"> -->
+    <script src="./plugins/popper.min.js"></script>
+
+    <nav class="navbar navbar-expand-lg fixed-top">
+      <div class="container-fluid">
+        <img src="logo.png" alt="Logo"class="navbar-brand" height="70" width="70">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="nav-title">Our Lady of the Roses Montessori Learning Center</div>
+
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item ">
+              <a class="nav-link flex-center" aria-current="page" href="home.php">
+                <i class="material-icons " style="font-size: 25px;">home</i>
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="aboutus.php">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link cursor-pointer" id="loginbtn" data-bs-toggle="modal" data-bs-target="#loginModal">Profile</a>
+            </li> 
+          </ul>
+        </div>
+      </div>
+    </nav>  
+
+</head>
+      <style>
+        h1{
+            font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+            font-size: 55px;
+            text-align: left;
+            margin-left: 0px;
+            color: antiquewhite;
+        }
+        
+      </style>   
+
+  <body >
+
+
+
+
+    <div class="content ">
+    <h1>Welcome to Our Lady of the <br>
+    Roses Montessori Learning Center</h1>
+    <p>
+      Online Enrollment System
+    </p>
+
+    <button>
+
+    </button>
+    
+    </div>
+     
+
+    
+    <script src="./plugins/bootstrap.bundle.min.js"></script>
+    <script src="./plugins/bootstrap.min.js"></script>
+
+  </body> 
+
+</html>
