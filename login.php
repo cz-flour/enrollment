857fb710a0a2cd38cfd796453f2a42b596d07f4f
Czarina Aicah Lopez <128132912+cz-flour@users.noreply.github.com>
2023-09-06 22:07:25 +0800

    login.php

--- login.php
+++ login.php
@@ -1,23 +1,44 @@
 <?php
 include_once "connection.php";
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Assuming that you have obtained the email and password from the user input
+$email = $_POST['email'];
+$pwd = $_POST['pwd'];
+
+// Validate email and password presence
+if (empty($email) || empty($pwd)) {
+    echo "Email and password are required.";
+    exit; // Terminate the script
 }
 
-// Validate email and password
-$sql = "SELECT * FROM user WHERE email = '$email'";
-$result = $conn->query($sql);
+// Prepare and execute the SQL query
+$sql = "SELECT * FROM user WHERE email = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $email);
+$stmt->execute();
+
+$result = $stmt->get_result();
 
 if ($result && $result->num_rows === 1) {
     $row = $result->fetch_assoc();
-    $hashedPassword = $row["pwd"];
+    $storedPassword = $row["pwd"]; // Assuming the column name is "pwd"
+    $is_admin = $row["is_admin"]; // Assuming the column name is "is_admin"
 
-    // Check if the provided password matches the stored hashed password
-    if (password_verify($password, $hashedPassword)) {
-        // Successful login
-        echo "Login successful!";
+    // Check if the provided password matches the stored password
+    if ($pwd === $storedPassword) {
+        $stmt->close();
+        $conn->close();
+
+        // Start or resume the session before any output
+        session_start();
+        $_SESSION['user_id'] = $row['user_id']; // Store user's ID in the session
+        
+        if ($is_admin == 1) {
+            header("location: admin/index.php"); // Redirect admin to index.php (admin page)
+        } else {
+            header("location: home2.php"); // Redirect other users to home2.php (user page)
+        }
+        exit; // Terminate the script after header redirection
     } else {
         // Invalid password
         echo "Invalid password.";
@@ -27,7 +48,7 @@ if ($result && $result->num_rows === 1) {
     echo "Invalid email.";
 }
 
-// Close connection
+// Close statement and connection
+$stmt->close();
 $conn->close();
-
 ?>
