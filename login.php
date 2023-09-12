9044b6ff42fdb9a604921591158d0e3c9043943a
Czarina Aicah Lopez <128132912+cz-flour@users.noreply.github.com>
2023-09-12 13:13:18 +0800

    login.php

--- login.php
+++ login.php
@@ -1,54 +1,44 @@
 <?php
 include_once "connection.php";
 
-// Assuming that you have obtained the email and password from the user input
 $email = $_POST['email'];
 $pwd = $_POST['pwd'];
 
-// Validate email and password presence
 if (empty($email) || empty($pwd)) {
     echo "Email and password are required.";
-    exit; // Terminate the script
-}
-
-// Prepare and execute the SQL query
-$sql = "SELECT * FROM user WHERE email = ?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("s", $email);
-$stmt->execute();
-
-$result = $stmt->get_result();
-
-if ($result && $result->num_rows === 1) {
-    $row = $result->fetch_assoc();
-    $storedPassword = $row["pwd"]; // Assuming the column name is "pwd"
-    $is_admin = $row["is_admin"]; // Assuming the column name is "is_admin"
-
-    // Check if the provided password matches the stored password
-    if ($pwd === $storedPassword) {
-        $stmt->close();
-
-        // Start or resume the session before any output
-        session_start();
-
-        $_SESSION['user_id'] = $row['user_id']; // Store user's ID in the session
-        
-        if ($is_admin == 1) {
-            header("location: admin/index.php"); // Redirect admin to index.php (admin page)
-        } else {
-            header("location: home2.php"); // Redirect other users to home2.php (user page)
-        }
-        exit; // Terminate the script after header redirection
-    } else {
-        // Invalid password
-        echo "<script>alert('Invalid Password')</script>.";
-    }
 } else {
-    // Invalid email
-    echo "Invalid email.";
+    $sql = "SELECT * FROM user WHERE email = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result && $result->num_rows === 1) {
+        $row = $result->fetch_assoc();
+        $storedPassword = $row["pwd"]; // Assuming the column name is "pwd"
+
+        if (password_verify($pwd, $storedPassword)) { // Use password_verify to check the password
+            session_start();
+            $_SESSION['user_id'] = $row['user_id'];
+            $is_admin = $row["is_admin"]; // Assuming the column name is "is_admin"
+
+            if ($is_admin == 1) {
+                header("location: admin/index.php");
+            } else {
+                header("location: home2.php");
+            }
+            exit;
+        } else {
+            // Invalid password
+            echo "Invalid password";
+        }
+    } else {
+        // Invalid email
+        echo "Invalid email.";
+    }
+
+    $stmt->close();
+    $conn->close();
 }
 
-// Close statement and connection
-$stmt->close();
-$conn->close();
 ?>
